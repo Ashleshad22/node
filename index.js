822eaa7a9ff6c70dee1eaa605f15102c3b3ece1a
= <ashlesha.deshpande@skf.com>
2024-06-25 09:33:34 +0530

    node commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send("Hello from home page")
+});
+
+app.get('/about', (req, res) => {
+    return res.send("Hello from about page" + " hey " + req.query.name);
+});
+
+
+
+app.listen(8001, () => {console.log('Server started!')});
+
